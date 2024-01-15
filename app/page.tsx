46d732aab8beda5339494ae40f2a4d278925f390
Denys Tomashivskyi <tomashivskuy.54@gmail.com>
2024-01-15 18:46:21 +0200

    anythign

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ interface HomeProps {
   searchParams: IListingsParams;
 }
 
+export const dynamic = "force-dynamic";
+
 const Home = async ({ searchParams }: HomeProps) => {
   const currentUser = await getCurrentUser();
 
